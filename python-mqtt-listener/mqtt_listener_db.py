fd42cf0e3f9785a4525f7f9bbe080f0ffce73e45
up <upperbound.work@gmail.com>
2021-04-09 22:40:54 +0700

    add mqtt listner to save into db

--- python-mqtt-listener/mqtt_listener_db.py
+++ python-mqtt-listener/mqtt_listener_db.py
@@ -1,17 +1,26 @@
 import paho.mqtt.client as mqtt
+from Mqtt_db import Mqtt_db
 
-host = "34.87.54.201"
-port = "1883"
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("/ESP/#")
 
-def on_connect(self, client, userdata, rc):
-    print("MQTT Connected.")
-    self.subscribe("TEST/MQTT")
-
-def on_message(client, userdata, msg):
-    print(msg.payload.decode("utf-8","strict"))
-
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(host)
-client.loop_forever()
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):            
+    print(msg.topic+" "+str(msg.payload))   
+    db = Mqtt_db()
+    db.show(msg.topic,msg.payload)
+    
+if __name__ == '__main__':
+    client = mqtt.Client()    
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("34.87.54.201", 1883, 60)
+    # Blocking call that processes network traffic, dispatches callbacks and
+    # handles reconnecting.
+    # Other loop*() functions are available that give a threaded interface and a
+    # manual interface.
+    client.loop_forever()
